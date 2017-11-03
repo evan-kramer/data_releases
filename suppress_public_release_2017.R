c0ab50742ac5ac899d1d55d43d4e2bc8cea2348a
evan-kramer <evan.kramer@tn.gov>
2017-11-03 09:09:29 -0500

    Corrected suppression rules where some schools were being over-suppressed

--- suppress_public_release_2017.R
+++ suppress_public_release_2017.R
@@ -16,11 +16,17 @@ system_release = read_excel("system_release_2017_JW_10112017_formatted.xlsx") %>
     transmute(Year, System, `System Name`, Subject, Subgroup, Grade, `Valid Tests`,
               `N Below`, `Percent Below`, `N Approaching`, `Percent Approaching`,
               `N On Track/Mastered` = `N On Track` + `N Mastered`, `Percent On Track/Mastered`) %>% 
-    mutate_each(funs(ifelse(`Percent Below` > 99 | `Percent Below` < 1 | `Percent Approaching` > 99 | 
-                                `Percent Approaching` < 1 | `Percent On Track/Mastered` > 99 | 
-                                `Percent On Track/Mastered` <  1, "**", as.character(.))), `N Below`:`Percent Approaching`) %>% 
-    mutate_each(funs(ifelse(`Percent On Track/Mastered` > 99 | `Percent On Track/Mastered` < 1, "**", as.character(.))), ends_with("/Mastered")) %>% 
-    mutate_each(funs(ifelse(`Valid Tests` < 10, "*", as.character(.))), `N Below`:`Percent On Track/Mastered`) 
+    mutate_each(funs(ifelse(`Percent On Track/Mastered` < 1 | `Percent On Track/Mastered` > 99, "**", as.character(.))), 
+                starts_with("N"), starts_with("Percent")) %>% 
+    mutate_each(funs(ifelse(. == "**", ., 
+                            ifelse(as.numeric(.) < 1 | as.numeric(.) > 99, "**", .))), `Percent Below`, `Percent Approaching`) %>% 
+    mutate_each(funs(ifelse(`Percent Below` != "**" & `Percent On Track/Mastered` != "**" & `Percent Approaching` == "**",
+                            "**", .)), `N Approaching`, ends_with("Below")) %>%
+    mutate_each(funs(ifelse(`Percent Approaching` != "**" & `Percent On Track/Mastered` != "**" & `Percent Below` == "**",
+                            "**", .)), `N Below`, ends_with("Approaching")) %>%
+    mutate_each(funs(ifelse(`Valid Tests` < 10, "*", as.character(.))), `N Below`:`Percent On Track/Mastered`) %>%
+    mutate(`N Below` = ifelse(`Percent Below` == "**", "**", `N Below`),
+           `N Approaching` = ifelse(`Percent Approaching` == "**", "**", `N Approaching`)) 
 
 write_csv(system_release, "K:/ORP_accountability/data/2017_final_accountability_files/suppressed_system_release_3-8_oct17.csv", na = "")
 
@@ -30,10 +36,25 @@ school_release = read_excel("school_release_2017_JW_10112017_formatted.xlsx") %>
     transmute(Year, System, `System Name`, School, `School Name`, Subject, Subgroup, Grade, 
               `Valid Tests`, `N Below`, `Percent Below`, `N Approaching`, `Percent Approaching`,
               `N On Track/Mastered` = `N On Track` + `N Mastered`, `Percent On Track/Mastered`) %>% 
-    mutate_each(funs(ifelse(`Percent Below` > 95 | `Percent Below` < 5 | `Percent Approaching` > 95 | 
-                                `Percent Approaching` < 5 | `Percent On Track/Mastered` > 95 | 
-                                `Percent On Track/Mastered` <  5, "**", as.character(.))), `N Below`:`Percent Approaching`) %>% 
-    mutate_each(funs(ifelse(`Percent On Track/Mastered` > 95 | `Percent On Track/Mastered` < 5, "**", as.character(.))), ends_with("/Mastered")) %>% 
-    mutate_each(funs(ifelse(`Valid Tests` < 10, "*", as.character(.))), `N Below`:`Percent On Track/Mastered`) 
+    mutate_each(funs(ifelse(`Percent On Track/Mastered` < 5 | `Percent On Track/Mastered` > 95, "**", as.character(.))), 
+                starts_with("N"), starts_with("Percent")) %>% 
+    mutate_each(funs(ifelse(. == "**", ., 
+                            ifelse(as.numeric(.) < 5 | as.numeric(.) > 95, "**", .))), `Percent Below`, `Percent Approaching`) %>% 
+    mutate_each(funs(ifelse(`Percent Below` != "**" & `Percent On Track/Mastered` != "**" & `Percent Approaching` == "**",
+                            "**", .)), `N Approaching`, ends_with("Below")) %>%
+    mutate_each(funs(ifelse(`Percent Approaching` != "**" & `Percent On Track/Mastered` != "**" & `Percent Below` == "**",
+                            "**", .)), `N Below`, ends_with("Approaching")) %>%
+    mutate_each(funs(ifelse(`Valid Tests` < 10, "*", as.character(.))), `N Below`:`Percent On Track/Mastered`) %>%
+    mutate(`N Below` = ifelse(`Percent Below` == "**", "**", `N Below`),
+           `N Approaching` = ifelse(`Percent Approaching` == "**", "**", `N Approaching`))
+
 
 write_csv(school_release, "K:/ORP_accountability/data/2017_final_accountability_files/suppressed_school_release_oct17.csv", na = "")
+
+
+system_release %>% 
+    filter(as.numeric(`N Below`) / as.numeric(`Valid Tests`) < .01 | as.numeric(`N Below`) / as.numeric(`Valid Tests`) > .99 | 
+               as.numeric(`N Approaching`) / as.numeric(`Valid Tests`) < .01 | as.numeric(`N Approaching`) / as.numeric(`Valid Tests`) > .99 | 
+               as.numeric(`N On Track/Mastered`) / as.numeric(`Valid Tests`) < .01 | as.numeric(`N On Track/Mastered`) / as.numeric(`Valid Tests`) > .99) %>% 
+    View()
+
